ce6a55eb16417fe6c676d8b1de2ed7ff886775ac
Nicolas Blanc <sinfomicien@gmail.com>
2014-09-11 12:52:13 +0200

    Fix PaxHeaders when packaging (fix #2)

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'nicolas.blanc@blablacar.com'
 license 'Apache 2.0'
 description 'Installs/Configures MariaDB'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 supports 'ubuntu'
-supports 'debian', '~> 7'
-supports 'centos', '~> 6'
+supports 'debian', '>= 7.0'
+supports 'centos', '>= 6.4'
 
 depends 'apt'
 depends 'yum'
