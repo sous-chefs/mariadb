e95892cf181b3b52288a4cadf2f6b9e1dcf689a4
Nicolas Blanc <sinfomicien@gmail.com>
2014-09-29 17:03:07 +0200

    Correct Rubocop Offenses

--- providers/replication.rb
+++ providers/replication.rb
@@ -11,16 +11,19 @@ end
 
 action :add do
   sql_string = 'CHANGE MASTER '
-  sql_string += '\'' + new_resource.name + '\' ' if new_resource.name != 'default'
+  sql_string += '\'' + new_resource.name + \
+    '\' ' if new_resource.name != 'default'
   sql_string += 'TO '
   sql_string += 'MASTER_HOST=\'' + new_resource.master_host + '\', '
-  sql_string += 'MASTER_PORT=' + new_resource.master_port + ', ' if new_resource.master_port != nil
+  sql_string += 'MASTER_PORT=' + new_resource.master_port + \
+    ', ' unless new_resource.master_port.nil?
   sql_string += 'MASTER_USER=\'' + new_resource.master_user + '\', '
   sql_string += 'MASTER_PASSWORD=\'' + new_resource.master_password + '\''
   if new_resource.master_use_gtid == 'no'
     # Use non GTID replication setup
-    if new_resource.master_log_file != nil
-      sql_string += ', MASTER_LOG_FILE=\'' + new_resource.master_log_file + '\''
+    unless new_resource.master_log_file.nil?
+      sql_string += ', MASTER_LOG_FILE=\'' + \
+        new_resource.master_log_file + '\''
       sql_string += ', MASTER_LOG_POS=' + new_resource.master_log_pos
     end
   else
@@ -35,8 +38,8 @@ end
 
 action :remove do
   execute 'remove_replication_from_master_' + new_resource.name do
-   command '/bin/echo "STOP SLAVE \'' + new_resource.name + '\'; ' \
-     'RESET SLAVE \'' + new_resource.name + '\' ALL; ' \
-     ';" | /usr/bin/mysql'
-   end
+    command '/bin/echo "STOP SLAVE \'' + new_resource.name + '\'; ' \
+      'RESET SLAVE \'' + new_resource.name + '\' ALL; ' \
+      ';" | /usr/bin/mysql'
+  end
 end
--- resources/replication.rb
+++ resources/replication.rb
@@ -8,11 +8,17 @@ default_action :add
 
 # name of the extra conf file, used for .cnf filename
 attribute :connection_name, kind_of: String, name_attribute: true
-attribute :master_host, kind_of: String, :required => true
-attribute :master_user, kind_of: String, :required => true
-attribute :master_password, kind_of: String, :required => true
-attribute :master_connect_retry, kind_of: [String,nil], :default => nil
-attribute :master_port, kind_of: [Integer,nil], :default => nil
-attribute :master_log_pos, kind_of: [Integer,nil], :default => nil
-attribute :master_log_file, kind_of: [String,nil], :default => nil
-attribute :master_use_gtid, kind_of: ['current_pos', 'slave_pos', 'no'], :default => 'no'
+attribute :master_host, kind_of: String, required: true
+attribute :master_user, kind_of: String, required: true
+attribute :master_password, kind_of: String, required: true
+attribute :master_connect_retry, kind_of: [String, nil], default: nil
+attribute :master_port, kind_of: [Integer, nil], default: nil
+attribute :master_log_pos, kind_of: [Integer, nil], default: nil
+attribute :master_log_file, kind_of: [String, nil], default: nil
+attribute :master_use_gtid,
+          kind_of: %w(
+            current_pos
+            slave_pos
+            no
+          ),
+          default: 'no'
