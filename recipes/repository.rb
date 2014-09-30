e5e5b6311797d6089315e3c3d2ee8afbcb82f166
Nicolas Blanc <sinfomicien@gmail.com>
2014-09-30 17:53:52 +0200

    Add EPEL repository for Redhat/Centos platform as socat is needed by Galera

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -28,6 +28,15 @@ if node['mariadb']['use_default_repository']
       gpgkey 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
       action :create
     end
+
+    # add the EPEL repo
+    yum_repository 'epel' do
+      description 'Extra Packages for Enterprise Linux'
+      mirrorlist 'http://mirrors.fedoraproject.org/' \
+                 'mirrorlist?repo=epel-6&arch=$basearch'
+      gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+      action :create
+    end
   else
   end
 end
