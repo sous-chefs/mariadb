1aaba7b7c7622a04b91685531fa9e87f1e457069
Nicolas Blanc <sinfomicien@gmail.com>
2014-11-27 13:44:01 +0100

    Correct bad quote in system call

--- recipes/_audit_plugin.rb
+++ recipes/_audit_plugin.rb
@@ -20,12 +20,12 @@ audit_plugin_options['enable'] = '#server_audit_logging = ON'
 execute 'install_mariadb_audit_plugin' do
   command '/usr/bin/mysql -e "INSTALL PLUGIN server_audit ' + \
     'SONAME \'server_audit\';"'
-  notifies :run, 'execute[configure_mariadb_audit_plugin]', :immediate
+  notifies
   not_if do
     cmd = Mixlib::ShellOut.new('/usr/bin/mysql -u root -B -N -e "SELECT 1 ' + \
                     'FROM information_schema.plugins ' + \
-                    'WHERE PLUGIN_NAME = \"SERVER_AUDIT\"' + \
-                    'AND PLUGIN_STATUS = \"ACTIVE\";')
+                    'WHERE PLUGIN_NAME = \'SERVER_AUDIT\'' + \
+                    'AND PLUGIN_STATUS = \'ACTIVE\';"')
     cmd.run_command
     cmd.stdout == '1'
   end
