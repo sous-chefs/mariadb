bb391dc0ee33524f99ea3eb79be24f5b89990a58
Cesare Bellini <cesare.bellini@yakara.com>
2014-12-09 16:09:35 +0000

    Use yum-epel cookbook
    Ensures correct EPEL version is used.

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -29,14 +29,6 @@ if node['mariadb']['use_default_repository']
       action :create
     end
 
-    # add the EPEL repo
-    yum_repository 'epel' do
-      description 'Extra Packages for Enterprise Linux'
-      mirrorlist 'http://mirrors.fedoraproject.org/' \
-        'mirrorlist?repo=epel-6&arch=$basearch'
-      gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-      action :create
-    end
-  else
+    include_recipe 'yum-epel::default'
   end
 end
