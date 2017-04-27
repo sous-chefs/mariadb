e0e070010c2b9e579e5dc30b77c0644353adb8ff
Rich Theobald <rtheobald@virttrade.com>
2017-04-27 22:12:50 +0100

    version fix

--- recipes/galera.rb
+++ recipes/galera.rb
@@ -47,6 +47,12 @@ else
   end
 end
 
+if node['mariadb']['install']['version'] == '5.5' or node['mariadb']['install']['version'] == '10.0'
+  package_type='galera'
+else
+  package_type='server'
+end
+
 case node['mariadb']['install']['type']
 when 'package'
   # include MariaDB repositories
@@ -54,24 +60,24 @@ when 'package'
 
   case node['platform']
   when 'debian', 'ubuntu'
-    include_recipe "#{cookbook_name}::_debian_galera"
-  when 'redhat', 'centos', 'scientific', 'amazon'
-    include_recipe "#{cookbook_name}::_redhat_galera"
+    include_recipe "#{cookbook_name}::_debian_#{package_type}"
+  when 'redhat', 'centos', 'fedora', 'scientific', 'amazon'
+    include_recipe "#{cookbook_name}::_redhat_#{package_type}"
   end
 when 'from_source'
   # To be filled as soon as possible
 end
 
 if node['mariadb']['install']['extra_packages']
-  if node['mariadb']['galera']['wsrep_sst_method'] == 'rsync'
-    package 'rsync' do
-      action :install
-    end
+if node['mariadb']['galera']['wsrep_sst_method'] == 'rsync'
+  package 'rsync' do
+    action :install
+  end
   elsif node['mariadb']['galera']['wsrep_sst_method'] =~ /^xtrabackup(-v2)?/
     %w(percona-xtrabackup socat pv).each do |pkg|
       package pkg do
-        action :install
-      end
+      action :install
+    end
     end
   end
 end
@@ -79,44 +85,44 @@ end
 include_recipe "#{cookbook_name}::config"
 
 if node['mariadb']['galera']['gcomm_address'].nil?
-  galera_cluster_nodes = []
+galera_cluster_nodes = []
   if !node['mariadb'].attribute?('rspec') && Chef::Config[:solo]
-    if node['mariadb']['galera']['cluster_nodes'].empty?
-      Chef::Log.warn('By default this recipe uses search (unsupported by Chef Solo).' \
-                     ' Nodes may manually be configured as attributes.')
-    else
-      galera_cluster_nodes = node['mariadb']['galera']['cluster_nodes']
-    end
+  if node['mariadb']['galera']['cluster_nodes'].empty?
+    Chef::Log.warn('By default this recipe uses search (unsupported by Chef Solo).' \
+                   ' Nodes may manually be configured as attributes.')
   else
-    if node['mariadb']['galera']['cluster_search_query'].empty?
-      galera_cluster_nodes = search(
-        :node, \
-        "mariadb_galera_cluster_name:#{node['mariadb']['galera']['cluster_name']}"
-      )
-    else
-      galera_cluster_nodes = search 'node', node['mariadb']['galera']['cluster_search_query']
-      log 'Chef search results' do
-        message "Searching for \"#{node['mariadb']['galera']['cluster_search_query']}\" \
-          resulted in \"#{galera_cluster_nodes}\" ..."
-        level :debug
-      end
-    end
-    # Sort Nodes by fqdn
-    galera_cluster_nodes.sort! { |x, y| x[:fqdn] <=> y[:fqdn] }
+    galera_cluster_nodes = node['mariadb']['galera']['cluster_nodes']
   end
+else
+  if node['mariadb']['galera']['cluster_search_query'].empty?
+    galera_cluster_nodes = search(
+      :node, \
+      "mariadb_galera_cluster_name:#{node['mariadb']['galera']['cluster_name']}"
+    )
+  else
+    galera_cluster_nodes = search 'node', node['mariadb']['galera']['cluster_search_query']
+    log 'Chef search results' do
+      message "Searching for \"#{node['mariadb']['galera']['cluster_search_query']}\" \
+        resulted in \"#{galera_cluster_nodes}\" ..."
+        level :debug
+    end
+  end
+  # Sort Nodes by fqdn
+  galera_cluster_nodes.sort! { |x, y| x[:fqdn] <=> y[:fqdn] }
+end
 
-  first = true
-  gcomm = 'gcomm://'
-  galera_cluster_nodes.each do |lnode|
-    next unless lnode.name != node.name
-    gcomm += ',' unless first
+first = true
+gcomm = 'gcomm://'
+galera_cluster_nodes.each do |lnode|
+  next unless lnode.name != node.name
+  gcomm += ',' unless first
     gcomm += if String(lnode['mariadb']['galera']['wsrep_node_port']).empty?
                lnode['fqdn']
              else
                "#{lnode['fqdn']}:#{lnode['mariadb']['galera']['wsrep_node_port']}"
              end
-    first = false
-  end
+  first = false
+end
 else
   gcomm = node['mariadb']['galera']['gcomm_address']
 end
@@ -163,15 +169,15 @@ unless node['mariadb']['galera']['wsrep_provider_options'].empty?
   galera_options['wsrep_provider_options'] = wsrep_prov_opt
 end
 galera_options['wsrep_cluster_address'] = gcomm
-galera_options['wsrep_cluster_name'] = \
+galera_options['wsrep_cluster_name']    = \
   node['mariadb']['galera']['cluster_name']
-galera_options['wsrep_sst_method'] = \
+galera_options['wsrep_sst_method']      = \
   node['mariadb']['galera']['wsrep_sst_method']
 if node['mariadb']['galera'].attribute?('wsrep_sst_auth')
-  galera_options['wsrep_sst_auth'] = \
+  galera_options['wsrep_sst_auth']        = \
     node['mariadb']['galera']['wsrep_sst_auth']
 end
-galera_options['wsrep_provider'] = \
+galera_options['wsrep_provider']        = \
   node['mariadb']['galera']['wsrep_provider']
 galera_options['wsrep_slave_threads'] = if node['mariadb']['galera'].attribute?('wsrep_slave_threads')
                                           node['mariadb']['galera']['wsrep_slave_threads']
@@ -205,7 +211,7 @@ unless node['mariadb']['galera']['wsrep_node_incoming_address_interface'].empty?
   galera_options['wsrep_node_incoming_address'] = ipaddress_inc unless ipaddress_inc.empty?
 end
 
-galera_options['wsrep_slave_threads'] = node['cpu']['total'] * 4
+galera_options['wsrep_slave_threads']   = node['cpu']['total'] * 4
 node['mariadb']['galera']['options'].each do |key, value|
   galera_options[key] = value
 end
@@ -234,20 +240,20 @@ if platform?('debian', 'ubuntu')
 
   if node['mariadb']['server_root_password'].is_a?(String)
     grants_command += '--password=\'' + \
-      node['mariadb']['server_root_password'] + '\' '
+                      node['mariadb']['server_root_password'] + '\' '
   end
 
   grants_command += '-e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ' \
-    'DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, ' \
-    'INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY ' \
-    'TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, ' \
-    'REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ' \
-    'ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON ' \
-    ' *.* TO \'' + node['mariadb']['debian']['user'] + \
-    '\'@\'' + node['mariadb']['debian']['host'] + '\' ' \
-    'IDENTIFIED BY \'' + \
-    node['mariadb']['debian']['password'] + '\' WITH GRANT ' \
-    'OPTION"'
+                    'DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, ' \
+                    'INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY ' \
+                    'TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, ' \
+                    'REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ' \
+                    'ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON ' \
+                    ' *.* TO \'' + node['mariadb']['debian']['user'] + \
+                    '\'@\'' + node['mariadb']['debian']['host'] + '\' ' \
+                    'IDENTIFIED BY \'' + \
+                    node['mariadb']['debian']['password'] + '\' WITH GRANT ' \
+                    'OPTION"'
 
   execute 'correct-debian-grants' do
     command grants_command
