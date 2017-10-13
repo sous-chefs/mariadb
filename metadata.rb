dbf5cc0da31c7b411e1bfbbaa5e656c67fc354a0
Stephen Hoekstra <stephenhoekstra@gmail.com>
2017-10-13 22:11:42 +0200

    Update maintainer and maintainer email [skip ci]

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'mariadb'
-maintainer 'Nicolas Blanc'
-maintainer_email 'sinfomicien@gmail.com'
+maintainer 'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
 license 'Apache-2.0'
 description 'Installs/Configures MariaDB'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/sinfomicien/mariadb' if respond_to?(:source_url)
-issues_url 'https://github.com/sinfomicien/mariadb/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/sous-chefs/mariadb' if respond_to?(:source_url)
+issues_url 'https://github.com/sous-chefs/mariadb/issues' if respond_to?(:issues_url)
 chef_version '>= 12.6' if respond_to?(:chef_version)
 version '1.5.2'
 
