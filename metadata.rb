c1e76926e167001a5730ff5862e6d6ae275bcd00
Dan Webb <dan.webb@damacus.io>
2018-05-23 16:01:37 -0500

    Drop chef-12 support
    Resolves #203

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,17 @@
-name 'mariadb'
-maintainer 'Sous Chefs'
+name             'mariadb'
+maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license 'Apache-2.0'
-description 'Installs/Configures MariaDB'
+license          'Apache-2.0'
+description      'Installs/Configures MariaDB'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/sous-chefs/mariadb' if respond_to?(:source_url)
-issues_url 'https://github.com/sous-chefs/mariadb/issues' if respond_to?(:issues_url)
-chef_version '>= 12.6' if respond_to?(:chef_version)
-version '1.5.3'
+source_url       'https://github.com/sous-chefs/mariadb'
+issues_url       'https://github.com/sous-chefs/mariadb/issues'
+chef_version     '>= 13'
+version          '1.5.3'
 
 supports 'ubuntu'
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 6.4'
-supports 'redhat', '>= 7.0'
 
 depends 'apt'
 depends 'build-essential'
