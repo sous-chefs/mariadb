d67cbdf9aa8f76a3ace2f38e957135c7d84fb330
Nicolas Blanc <sinfomicien@gmail.com>
2018-06-25 18:47:16 +0200

    Correct user InSpec tests

--- test/integration/resources/controls/user_spec.rb
+++ test/integration/resources/controls/user_spec.rb
@@ -9,12 +9,12 @@ control 'mariadb_user' do
     its(:stdout) { should_not match(/kermit/) }
   end
 
-  describe sql.query('show grants for \'fozzie\'@\'mars\'') do
-    its(:stdout) { should contain '*EF112B3D562CB63EA3275593C10501B59C4A390D' }
+  describe sql.query("show grants for 'fozzie'@'mars'") do
+    its(:stdout) { should include '*EF112B3D562CB63EA3275593C10501B59C4A390D' }
   end
 
   describe sql.query('show grants for  \'moozie\'@\'127.0.0.1\'') do
-    its(:stdout) { should contain '*F798E7C0681068BAE3242AA2297D2360DBBDA62B' }
+    its(:stdout) { should include '*F798E7C0681068BAE3242AA2297D2360DBBDA62B' }
   end
 
   sql2 = mysql_session('moozie', 'zokkazokka', '127.0.0.1')
@@ -23,19 +23,19 @@ control 'mariadb_user' do
     its(:exit_status) { should eq 0 }
   end
 
-  describe sql.query('show grants for \'rowlf\'@\'127.0.0.1\'') do
-    its(:stdout) { should contain '*6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9' }
+  describe sql.query('show grants for \'rowlf\'@\'localhost\'') do
+    its(:stdout) { should include '*6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9' }
   end
 
-  describe sql.query('show grants for \'statler\'@\'127.0.0.1\'') do
-    its(:stdout) { should contain '*2027D9391E714343187E07ACB41AE8925F30737E' }
+  describe sql.query('show grants for \'statler\'@\'localhost\'') do
+    its(:stdout) { should include '*2027D9391E714343187E07ACB41AE8925F30737E' }
   end
 
   describe sql.query('select Host from mysql.user where User like \'gonzo\'') do
-    its(:stdout) { should contain '10.10.10.%' }
+    its(:stdout) { should include '10.10.10.%' }
   end
 
   describe sql.query('show grants for \'rizzo\'@\'127.0.0.1\'') do
-    its(:stdout) { should contain '*125EA03B506F7C876D9321E9055F37601461E970' }
+    its(:stdout) { should include '*125EA03B506F7C876D9321E9055F37601461E970' }
   end
 end
