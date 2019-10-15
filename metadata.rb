abe90b9d8a857c24a28d46a63cb090c2fd7fb4aa
Mark E. Schill <Mark.Schill@cmschill.net>
2019-10-14 21:10:52 -0400

    Additional style fixes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs/Configures MariaDB'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 source_url       'https://github.com/sous-chefs/mariadb'
 issues_url       'https://github.com/sous-chefs/mariadb/issues'
 chef_version     '>= 13'
